d383126169cebba74f022e0ea5786dbeac39c444
xlepotato <yucia181@gmail.com>
2019-11-12 15:41:43 +0800

    completed web scrape api

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ app.config["DEBUG"] = True
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = "/usr/bin/google-chrome"
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless") # run chrome on headless mode, no window GUI while scraping
 
-URL = "https://cashchanger.co/singapore"
 
 
 '''
@@ -33,22 +32,20 @@ def hello_world():
        '''
 
 
-
+'''
+    Function to display all the best rate of different currencies with respect to SGD
+'''
 @app.route('/api/v1/resources/currency/buy/bestrate/all', methods=['GET'])
 def buy_bestrate():
+    URL = "https://cashchanger.co/singapore"
     try:
-        # page = urllib.request.urlopen(url) # conntect to website
         r = requests.get(URL)
     except:
         print("An error occured.")
-        # soup = BeautifulSoup(page, 'html.parser')
     soup = BeautifulSoup(r.content, 'html.parser')
-    # print(soup.prettify) # gives the visual representation of the parse tree created from the raw HTML content.
     best_rate_container = soup.find('div', class_='container bestrate-container')
-    # print(best_rate_container.prettify)
     currencies = []
 
-    # bestrate_table = best_rate_container.find_all('div', class_='bestrate')
     for row in best_rate_container.find_all('div', class_='bestrate'):
         print(row.prettify)
         currency = {}
@@ -56,44 +53,20 @@ def buy_bestrate():
         currency['currency_code'] = row.find('span', class_='currency-code float-left').text
         currency['rate'] = row.find('div', class_='text-big text-center').text
         currencies.append(currency)
-    # print(bestrate_table.prettify)
 
-
-    # regex = re.compile('^tocsection-')
-    #     # content_lis = soup.find_all('li', attrs={'class': regex})
-    #     # print(content_lis)
-    #     # content = []
-    #     # for li in content_lis:
-    #     #     content.append(li.getText().split('\n')[0])
-    #     # print(content)
-    #     # return jsonify(content)
     return jsonify(currencies)
-    # return 'nyan'
-
-    # # Getting the keywords section
-    # keyword_section = soup.find(class_="keywords-section")
-    # # Same as: soup.select("div.article-wrapper grid row div.keywords-section")
-    #
-    # # Getting a list of all keywords which are inserted into a keywords list in line 7.
-    # keywords_raw = keyword_section.find_all(class_="keyword")
-    # keyword_list = [word.get_text() for word in keywords_raw]
 
 
-@app.route('/api/v1/resources/moneychanger/profile', methods=['GET'])
+@app.route('/api/v1/resources/moneychanger/profile', methods=['GET']) # Get a specific money changer info for testing purposes
 def get_profile():
     MONEYCHANGER_URL = "https://cashchanger.co/singapore/mc/simlim-exchange-and-trading/189"
     try:
         r = requests.get(MONEYCHANGER_URL)
     except:
         print("An error occured.")
-        # soup = BeautifulSoup(page, 'html.parser')
     soup = BeautifulSoup(r.content, 'html.parser')
-    # print(soup.prettify) # gives the visual representation of the parse tree created from the raw HTML content.
     profile = soup.find('div', class_='mc-detail')
-    # print(profile.prettify)
     details = []
-
-    # bestrate_table = best_rate_container.find_all('div', class_='bestrate')
     for row in profile.find_all('div', class_='profile-card box'):
         print(row.prettify)
         detail = {}
@@ -102,36 +75,20 @@ def get_profile():
         detail['tel_No'] = row.find('p', class_='js-intro-mc-phone-container contact').a['href']
         detail['mrt'] = row.find_all('p')[2].text
         detail['address'] = row.find('p', class_='js-intro-mc-address-container').text
-
         # Clean data
         detail['mrt'] = (detail['mrt'].replace("\n", "")).strip()
         detail['address'] = (detail['address'].replace("\n", "")).strip().partition("      ")[0]
         detail['operating_hours'] = (detail['operating_hours'].replace("\n", "")).strip().replace("  ", "")
-        # .replace("  ","")
-
         details.append(detail)
-    # print(bestrate_table.prettify)
     return jsonify(details)
 
 
-
-@app.route('/test', methods=['GET'])
-def get_p():
-    details = []
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver = webdriver.Chrome(r'C:\Users\WNG056\Downloads\chromedriver_win32\chromedriver.exe')
-    driver.get('https://cashchanger.co/singapore/mc/simlim-exchange-and-trading/189')
-    detail = {}
-    detail['img'] = driver.find_element_by_xpath(
-        '/html/body/div[1]/div/div[2]/div[1]/section[2]/div/div[1]/div/div/div[1]/img').get_attribute('src')
-    details.append(detail)
-    return jsonify(details)
 
 
 @app.route('/api/v1/resources/moneychanger/details', methods=['GET'])
 def get_moneychanger():
-    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=chrome_options)
-    # driver = webdriver.Chrome(r'C:\Users\WNG056\Downloads\chromedriver_win32\chromedriver.exe')
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=chrome_options) # Use the chromedriver installed
+    # driver = webdriver.Chrome(r'C:\Users\WNG056\Downloads\chromedriver_win32\chromedriver.exe') # Uncomment for localhost testing
     details = []
 
     for i in range(1, 212):
@@ -210,15 +167,18 @@ def get_moneychanger():
     return jsonify(details)  # return the data in json format
 
 
-
+'''
+    Function used for testing purposes in replace with the original function.
+    Only Iterate up to 20 different Money Changer web pages.
+'''
 
 @app.route('/api/v1/resources/moneychanger/test', methods=['GET'])
 def get_moneychanger_test():
-    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=chrome_options)
-    # driver = webdriver.Chrome(r'C:\Users\WNG056\Downloads\chromedriver_win32\chromedriver.exe')
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=chrome_options)  # Use the chromedriver installed
+    # driver = webdriver.Chrome(r'C:\Users\WNG056\Downloads\chromedriver_win32\chromedriver.exe') # Uncomment for localhost testing
     details = []
 
-    for i in range(1, 30):
+    for i in range(1, 21):
         driver.get('https://cashchanger.co/singapore/mc/firman-hah-international-exchange/' + str(i))
         mc_detail = driver.find_elements_by_class_name('mc-detail')
         detail = {}
@@ -292,7 +252,6 @@ def get_moneychanger_test():
         print(k)
     print(len(details))  # print the length of the data scrapped
     return jsonify(details)  # return the data in json format
-
 
 
 if __name__ == '__main__':
